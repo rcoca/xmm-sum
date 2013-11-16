1b9e1e6ea6b82733d7636c2ad3f14b9fe2affd7c
Razvan Coca <razvan.coca@gmail.com>
2013-11-16 13:26:04 +0200

    added excel translation

--- xmm.py
+++ xmm.py
@@ -3,6 +3,8 @@
 from xml.dom import minidom
 import sys,os,glob
 from optparse import OptionParser
+import pyExcelerator
+import datetime
 
 def walkTree(root,extra=True):
     yield root
@@ -134,15 +136,102 @@ def EnumeratePaths(xmltree):
             
     return paths
 
+def PreOrderWalkTree(root,depth=0):
+    yield root,depth
+    if root.hasChildNodes():
+        for f in root.childNodes:
+            for x,d in PreOrderWalkTree(f,depth=depth+1):
+                yield x,d
+
+def GetDepth(root):
+    d=0
+    for n,dp in PreOrderWalkTree(root):
+        if isTextNode(n):
+            d=max(d,dp)
+    return d
+def GetRootDepth(root):
+    d=None
+    for n,dp in PreOrderWalkTree(root):
+        if isTextNode(n):
+            if not d:d=dp
+            d=min(d,dp)
+    return d
+
+
+def CreateXLS(xmltree):
+    wb = pyExcelerator.Workbook()
+    Root = filter(lambda x:isTextNode(x),xmltree.firstChild.childNodes)[0].getAttribute('TEXT').encode('ascii')
+    sh = wb.add_sheet(Root)
+
+    color_list=[ 'aqua','orange','silver']#'white','grey' ,'red','lime','yellow','silver','white' ]
+    lineNo=2
+    paths =[]
+    Depth=GetDepth(xmltree)
+    depth=GetRootDepth(xmltree)
+    style   = pyExcelerator.XFStyle()
+
+    style.font.bold=True
+    style.font.outline=True
+    style.font.underline=True
+    style.font.name='Arial'
+    style.borders.right  = pyExcelerator.Formatting.Borders.THICK
+    style.borders.top    = pyExcelerator.Formatting.Borders.THICK
+    style.borders.bottom = pyExcelerator.Formatting.Borders.THICK
+    style.borders.left   = pyExcelerator.Formatting.Borders.THICK
+
+    for i in range(Depth-depth):sh.write(0,i,'Level %d'%(i+1),style)
+    sh.write(0,Depth-depth,"Schedule:",style)
+    sh.write(0,Depth+depth,"Detailed Schedule:",style)
+    sh.write(0,Depth+depth+2,"StartDate",style)
+    style.num_format_str = 'mm/dd/yyyy'
+    
+    sh.write(1,Depth+depth+2,datetime.datetime.now(),style)
+    #print "Depth=%d,%d"%(Depth,depth)
+    for node,colNo in PreOrderWalkTree(xmltree):
+        if not isTextNode(node):continue
+        node_text=node.getAttribute('TEXT').encode('ascii')
+        #print (colNo-depth)*' ',node_text,color_list[(lineNo%len(color_list))]
+        m=re.search("(^.*)\:\s*([0-9\.]+\s*[dhmw]\s*$)",node_text)
+
+        style   = pyExcelerator.XFStyle()
+        color=color_list[(lineNo%len(color_list))]
+        style.pattern.pattern_fore_colour= pyExcelerator.Formatting.get_colour_val(color)
+        style.pattern.pattern=pyExcelerator.Formatting.Pattern.SOLID_PATTERN
+
+        if m:
+            dur=ConvertToDay(m.group(2)).replace('d','')
+            node_text=m.group(1)
+            for i in range(Depth+(colNo-2*depth)+1):
+                sh.write(lineNo,i,' ',style)
+            style.borders.right  = pyExcelerator.Formatting.Borders.THIN
+            style.borders.top    = pyExcelerator.Formatting.Borders.THIN
+            style.borders.bottom = pyExcelerator.Formatting.Borders.THIN
+            sh.write(lineNo,Depth+(colNo-2*depth),dur,style)
+            if not node.hasChildNodes():
+                sh.write(lineNo,Depth+depth,dur,style)
+                sh.write(lineNo,Depth+depth+1,str(float(dur)*7.0/5.0),style)
+                startname=chr(ord('A')+Depth+depth+2)
+                colname=chr(ord('A')+Depth+depth+1)
+                style.num_format_str = 'mm/dd/yyyy'
+                formula=pyExcelerator.Formula('$%s2+$%s%d'%(startname,colname,lineNo+1))
+                sh.write(lineNo,Depth+depth+2,formula,style)
+                sh.col(Depth+depth+2).width = 0x24E1/2
+        style.borders.right  = pyExcelerator.Formatting.Borders.NO_LINE
+        style.borders.left  = pyExcelerator.Formatting.Borders.THIN
+        sh.write(lineNo,(colNo-depth),node_text,style)
+        sh.col(colNo-depth).width = 0x24E1
+        lineNo+=1
+    return wb
+
+def ConvertToDay(arg):
+    D={'d':1,'w':5,'h':1.0/8.0,'m':23.0}
+    val=arg
+    for scale in D.keys():
+        val=val.replace(scale,"*%f"%D[scale])
+    val=str(eval(val))
+    return val
     
 def UpdateSumEstim(xmltree):
-    def ConvertToDay(arg):
-        D={'d':1,'w':5,'h':1.0/8.0,'m':23.0}
-        val=arg
-        for scale in D.keys():
-            val=val.replace(scale,"*%f"%D[scale])
-        val=str(eval(val))
-        return val
     used=[]
     opqueue=[]
     nused=[]
@@ -172,14 +261,20 @@ if __name__=='__main__':
     if len(flist)>0:f=flist[0]
     parser=OptionParser()
     parser.add_option('-u','--update-sum',dest='USum')
-
+    parser.add_option('-t','--text',dest='Text')#default =f
     (options,args) = parser.parse_args()
-    if not options.USum:
+    if not options.USum and not options.Text:
         parser.print_help()
         exit(2)
-
-    xmltree=minidom.parse(options.USum)
-    UpdateSumEstim(xmltree)
-    mapstring=xmltree.toxml().replace('<?xml version="1.0" ?>','')
-    with open(options.USum,"wt+") as FD:
-        print >>FD,mapstring
+    elif options.USum:
+	xmltree=minidom.parse(options.USum)
+	UpdateSumEstim(xmltree)
+	mapstring=xmltree.toxml().replace('<?xml version="1.0" ?>','')
+	with open(options.USum,"wt+") as FD:
+	    print >>FD,mapstring
+    elif options.Text:
+	xmltree=minidom.parse(options.Text)
+	UpdateSumEstim(xmltree)
+        wb=CreateXLS(xmltree)
+        outxls=options.Text.replace('.mm','.xls')
+        wb.save(outxls)
